1bf1eaa7a2ba2aeeab6016cee949befe13298f49
Mateusz Mazur <crowstormofficial@gmail.com>
2018-02-16 12:30:45 +0100

    body-parser

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/api')
 
 //setup express app
 const app = express();
+
+//body parser
+app.use(bodyParser.json());
+
+//initialize routes
+app.use('/api', routes);
 
 app.get('/api', function(req, res){
     console.log('get req');
--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const router = express.Router();
+
+//get list of androids
+router.get('/androids', function (req, res) {
+    res.send({ type: 'GET' });
+});
+
+//add new android
+router.post('/androids', function (req, res) {
+    console.log(req.body);
+    res.send({
+        type: 'POST',
+        name: req.body.name,
+        weight: req.body.weight
+    });
+});
+
+router.put('/androids/:id', function (req, res) {
+    res.send({ type: 'PUT' });
+});
+
+router.delete('/androids/:id', function (req, res) {
+    res.send({ type: 'DELETE' });
+});
+
+module.exports = router;
